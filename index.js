456d1fdcdf4e48425809a8b091124cd7dff7cc61
max <maxim07092001@gmail.com>
2020-03-15 18:38:37 +0300

    new

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-let money = +prompt('Ваш бюджет на месяц?',''),
-    time = +prompt('Введите дату в формате YYYY-MM-DD',''),
-    needMoney = +prompt('Введите обязательную статью расходов в этом месяце',''),
-    howMany = +prompt('Во сколько обойдется?','');
+'use strict';
 
-    let appData = {
-        budget:money,
-        timeData:time,
-        expenses:{
-            needMoney:howMany,
-            
-            
-        },
-        income: [],
-        saving:false
+
+let money = +prompt ("Ваш бюджет на месяц?", ""),
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+    
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+
+for (let i = 0; i < 1; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 10) {
+
+        console.log ("done");
+
+        appData.expenses[a] = b;
+    } else {         
+           
+        console.log('eror');   
+        i--;     
+        
     }
 
-    console.log(appData.budget);
+};
+appData.moneyPerDay = appData.budget/30; 
+console.log(appData.moneyPerDay);
 
-    alert(money/30);
+if (appData.moneyPerDay < 100){
+    console.log('мало');
+}
+else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log(' нормально ');
+} else if (appData.moneyPerDay > 2000){
+    console.log('много ');
+}
+else{
+console.error('error');
+}
